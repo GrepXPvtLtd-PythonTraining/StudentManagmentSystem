ccc8e8143aac4a3d59829bb4bc1bf166e4fe389c
kundan tikale <kundantikale4@gmail.com>
2022-04-29 09:34:57 -0700

    Updating Menu For Student Management System

--- BACKOFFICE/Kundan_StudentManagementSystem.py
+++ BACKOFFICE/Kundan_StudentManagementSystem.py
@@ -1,58 +1,150 @@
-def CreateRegister():
-    file = open("studentinfo.txt", "a+")
+import os
+
+
+def createFile():
+    file = open("Studentinfo.txt", "a+")
     return file
 
-def WriteToRegister(f, name, age, mob_number):
-    f.write("{}|{}|{}\n".format(name, age, mob_number))
-    f.close()
 
-def ReadRegister():
-    n = open("studentinfo.txt")
-    print(n.read())
-
-
-def header():
-    print('''------------------------ \n  G. M. C. Chindwara.. \n------------------------''')
+def addToFile(file, studentId, name, age, phoneNumber):
+    file.write("{}|{}|{}|{}\n".format(studentId, name, age, phoneNumber))
+    file.flush()
+    file.seek(0)
+    file.close()
 
 
-def add(f):
-    print("Add New Student")
-    name = input("Enter Name :- ")
-    age = int(input("Enter Age :- "))
-    mob_number = int(input("Enter mob_number :- "))
-    WriteToRegister(f, name, age, mob_number)
-    print("New Student is added")
+def readFile():
+    file = open("Studentinfo.txt")
+    print(file.read())
+    file.close()
 
 
-#
-# def delete():
-#     name = input("Enter Name of Student:- ")
-#     print("Delete Student")
+def checkTheFile(f, studentname):
+    f = open("Studentinfo.txt", "r")
+    data = " "
+    data = f.readlines()
+    for line in data:
+        splitData = line.split("|")
+        rollNum = splitData[0]
+        name = splitData[1]
+        age = splitData[2]
+        phoneNumber = splitData[3]
+        if studentname == name:
+            print("{}|{}|{}|{}".format(rollNum, name, age, phoneNumber))
 
 
-# def search():
-#     name = input("Enter Name of Student:- ")
-#     print("It should display student Name", name)
+def checkIfStudentExit(f, studentname):
+    f = open("Studentinfo.txt", "r")
+    data = " "
+    data = f.readlines()
+    for line in data:
+        splitData = line.split("|")
+        rollNum = splitData[0]
+        name = splitData[1]
+        age = splitData[2]
+        phoneNumber = splitData[3]
+        if studentname == name:
+            print("\n{}|{}|{}|{}".format(rollNum, name, age, phoneNumber))
+            return True
+
+
+def updateStudentInfo():
+    fh_r = open("Studentinfo.txt", "r")
+    fh_w = open("temp.txt", "w")
+    student_name = input("Enter The Student Name to Update = ")
+    data = " "
+    while data:
+        data = fh_r.readline()
+        l = data.split("|")
+        for i in range(0, len(l) - (len(l) - 1)):
+            roll_no = l[0]
+            if len(data) > 0:
+                if l[1] == student_name:
+                    name = input("Enter New Name = ")
+                    age = int(input("Enter New Age = "))
+                    ph = int(input("Enter New Phone NUmber = "))
+                    fh_w.write("{}|{}|{}|{}\n".format(roll_no, name, age, ph))
+                else:
+                    fh_w.write(data)
+    fh_w.close()
+    fh_r.close()
+    os.remove("Studentinfo.txt")
+    os.rename("temp.txt", "Studentinfo.txt")
+
+
+def delete():
+    fh_r = open("Studentinfo.txt", "r")
+    fh_w = open("temp.txt", "w")
+    student_name = input("Enter The Student Name to Delete The Record = ")
+    data = " "
+    while data:
+        data = fh_r.readline()
+        l = data.split("|")
+        for i in range(0, len(l) - (len(l) - 1)):
+            roll_no = l[0]
+            if len(data) > 0:
+                if l[1] != student_name:
+                    fh_w.write(data)
+
+    fh_w.close()
+    fh_r.close()
+    os.remove("Studentinfo.txt")
+    os.rename("temp.txt", "Studentinfo.txt")
 
 
 def choice():
-    print(''' 1.Add New Student\n 2.Delete Student\n 3.Search Student''')
-    choice = int(input("Enter You choice :- "))
+    print("******** Student Management System ********")
+    print(
+        ''' 1.Add New Student\n 2.Delete Student\n 3.Search Student \n 4.List Student \n 5.Update Student Information 
+        \n''')
+    choice = int(input("       Enter You choice :- "))
     return choice
 
 
-if _name_ == "_main_":
-    f = CreateRegister()
+def add(file):
+    print("*********Add student information********* ")
+    f = open("Studentinfo.txt", "r")
+    studentId = len(f.readlines()) + 1
+    name = input("Enter Student Name = ")
+    age = int(input("Enter Age = "))
+    phoneNumber = int(input("Enter Phone Number = "))
+    check = checkIfStudentExit(file, name)
+    if check is True:
+        print("student already exist ")
+        return False
+    else:
+        addToFile(file, studentId, name, age, phoneNumber)
+
+
+if __name__ == '__main__':
+    f = createFile()
     ch = choice()
     if ch == 1:
-        header()
-        add(f)
-        ReadRegister()
+        result = add(f)
+        if result is False:
+            pass
+        else:
+            print("\n")
+            print("----New Student added-----")
+            readFile()
     elif ch == 2:
-        # header()
-        # delete()
+        f.close()
+        print("\n")
+        readFile()
+        delete()
+        print("\n")
+        print("File After Deletion")
+        readFile()
     elif ch == 3:
-        # header()
-        # search()
+        Name = input("Enter the Name You Want To Search = ")
+        checkTheFile(f, Name)
+    elif ch == 4:
+        readFile()
+    elif ch == 5:
+        readFile()
+        f.close()
+        updateStudentInfo()
+        print("\n")
+        print("File After Update")
     else:
-        print("Invalid Choice")
+        print("Enter A Valid Option")

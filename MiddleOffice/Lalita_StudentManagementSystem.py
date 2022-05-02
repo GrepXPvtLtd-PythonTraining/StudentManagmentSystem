727327055134fefd39a60cdd8c5af8ed22c0a268
lalitarakshak <lalitar572.com>
2022-05-03 00:06:41 +0530

    update menu for Student Management System

--- MiddleOffice/Lalita_StudentManagementSystem.py
+++ MiddleOffice/Lalita_StudentManagementSystem.py
@@ -1,32 +1,58 @@
-def choose():
-    print("======================================")
-    print("Name of College:")
-    print("======================================")
-    print("choose your option")
-    print("Add New Student")
-    print("Delete Student")
-    print("Search Student")
-def Add():
-    print("========================================")
-    print("Add New Student")
-    print("========================================")
-    name=input("Enter Name:")
-    age=input("Enter Age:")
-    phoneNumber=input("Enter PhoneNumber:")
-    print("New Student is Added")
-def Delete():
-    print("=========================================")
-    print("Delete Student")
-    print("=========================================")
-    name=input("Enter Name of Student:")
-    print("Student is deleted")
-def Search():
-    print("=========================================")
-    print("Search Student")
-    print("=========================================")
-    name=input("Enter Name of Student:")
-    print("Name:",name)
-choose()
-Add()
-Delete()
-Search()
+import sqlite3
+
+
+def CreateTable():
+    c.execute("""create table StudentData(
+                     First_Name TEXT,
+                     Middle_name TEXT,
+                     Last_Name TEXT,
+                     age INTEGER,
+                     Phone_Number INTEGER
+                     )""")
+
+    conn.commit()
+
+
+def NewStudent():
+    First_name = input(" enter student name : ")
+    Middle_name = input(" enter middle name : ")
+    Last_name = input(" enter last name : ")
+    age = int(input(" enter  student age : "))
+    Phone_number = int(input(" enter phone number : "))
+    c.execute("INSERT INTO StudentData VALUES('{}','{}','{}',{},{})".format(First_name, Middle_name, Last_name,age,Phone_number))
+    conn.commit()
+    print(" student is added ")
+    SearchStudent()
+
+
+
+def DeleteStudent():
+    First_name = input("enter the first name : ")
+    Last_name = input("enter the last name : ")
+    c.execute("delete from StudenData where First_name = '{}', Last_name = '{}'".format(First_name, Middle_name))
+    conn.commit()
+
+
+def SearchStudent():
+    First_name = input("enter the first name for searched : ")
+    c.execute("select * from StudentData where First_name = '{}'".format(First_name))
+    print(c.fetchall())
+    conn.commit()
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect('StudentData.db')
+    c = conn.cursor()
+    CreateTable()
+    print("1. New student \n 2.delete Student \n 3.search Student ")
+    choice = int(input(" enter your choice : "))
+    if choice == 1:
+        NewStudent()
+
+    elif choice == 2:
+        DeleteStudent()
+    elif choice == 3:
+        SearchStudent()
+    else:
+        print(" invalid choice : ")
+    conn.close()
